698ac397d41b7052ce879a59b4f6136e4a1338b6
wandoujia <87866830+wandoujia555@users.noreply.github.com>
2024-07-29 09:17:03 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<script>
+	import config from "./config.js"
+	uni.login({
+		success(res) {
+			console.log(res.code)
+			uni.request({
+				url:"http://"+config.server.address+"/login",
+				data:{
+					code:res.code
+				},
+				success({data}){
+					uni.setStorageSync('openid',data)
+					console.log(data)
+				}
+			})
+		}
+	})
+	
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
